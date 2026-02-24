20e0c778f515f70622296f68227d46c467f137a5
divyasachdeva-netizen <divyasachdeva@phy.iith.ac.in>
2026-02-24 12:35:56 +0530

    Update plot_utils.py

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,1 +1,8 @@
+import matplotlib.pyplot as plt
 
+def plot_spectrum(E, F, xlabel="Energy (eV)", ylabel="Flux"):
+    plt.loglog(E, F)
+    plt.grid(True, ls="--", alpha=0.4)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
