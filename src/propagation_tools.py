b86404f2fd359ec7aaa3bab8e43f9abd1d14907a
divyasachdeva-netizen <divyasachdeva@phy.iith.ac.in>
2026-02-24 12:35:43 +0530

    Update propagation_tools.py

--- src/propagation_tools.py
+++ src/propagation_tools.py
@@ -1,1 +1,8 @@
+import crpropa
+from crpropa import ModuleList
 
+def setup_simple_propagation(maxlen=3000 * crpropa.Mpc):
+    """Returns a CRPropa ModuleList for straight-line photon propagation."""
+    sim = ModuleList()
+    sim.add(crpropa.SimplePropagation(maxlen))
+    return sim
